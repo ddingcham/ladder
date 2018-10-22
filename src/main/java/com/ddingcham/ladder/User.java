3be2d9333b3609bd5b32ae2ca88690fbaaa4242b
ddingcham <qo0op308@gmail.com>
2018-10-22 22:01:03 +0900

    fix User // testCase : user's name is empty

--- src/main/java/com/ddingcham/ladder/User.java
+++ src/main/java/com/ddingcham/ladder/User.java
@@ -4,7 +4,7 @@ public class User {
 	private String name;
 	
 	public User(String name){
-		if(name == null) throw new IllegalArgumentException("참여하는 사람의 이름은 null이 될 수 없습니다.");
+		if(name == null || name.isEmpty()) throw new IllegalArgumentException("참여하는 사람의 이름은 null이 될 수 없습니다.");
 		this.name = name;
 	}
 
--- src/test/java/com/ddingcham/ladder/UserTest.java
+++ src/test/java/com/ddingcham/ladder/UserTest.java
@@ -3,9 +3,6 @@ package com.ddingcham.ladder;
 import org.junit.Test;
 
 public class UserTest {
-
-	private static final int MAX_USER_NAME_LENGTH = 5;
-	private static final int MIN_USER_NAME_LENGTH = 1;
 	
 	@Test
 	public void 참여하는_사람의_이름이_정상적인_경우(){
@@ -23,7 +20,7 @@ public class UserTest {
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void 참여하는_사람의_이름이_1글자가_안되는_경우(){
+	public void 참여하는_사람의_이름이_빈_문자열일_경우(){
 		final String name = "";
 		new User(name);
 	}
