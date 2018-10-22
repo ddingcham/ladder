149c5785387b41d6b9eb66ad9f6376ea0c21caea
ddingcham <qo0op308@gmail.com>
2018-10-22 22:06:09 +0900

    fix User // testCase : user's name is over 5

--- src/main/java/com/ddingcham/ladder/User.java
+++ src/main/java/com/ddingcham/ladder/User.java
@@ -1,10 +1,11 @@
 package com.ddingcham.ladder;
 
 public class User {
+	private static final int MAX_USER_NAME_LENGTH = 5;
 	private String name;
 	
 	public User(String name){
-		if(name == null || name.isEmpty()) throw new IllegalArgumentException("참여하는 사람의 이름은 null이 될 수 없습니다.");
+		if(name == null || name.isEmpty() || name.length() > MAX_USER_NAME_LENGTH) throw new IllegalArgumentException("참여하는 사람의 이름 양식이 올바르지 않습니다.");
 		this.name = name;
 	}
 
--- src/test/java/com/ddingcham/ladder/UserTest.java
+++ src/test/java/com/ddingcham/ladder/UserTest.java
@@ -4,18 +4,18 @@ import org.junit.Test;
 
 public class UserTest {
 	
+	private static final String MAX_LENGTH_USER_NAME = "다섯글자명";
+	private static final String MIN_LENGTH_USER_NAME = "명";
+	
 	@Test
-	public void 참여하는_사람의_이름이_정상적인_경우(){
-		final String maxLengthName = "";
-		final String minLengthName = "";
-		
-		new User(maxLengthName);
-		new User(minLengthName);
+	public void 참여하는_사람의_이름이_정상적인_경우(){		
+		new User(MAX_LENGTH_USER_NAME);
+		new User(MIN_LENGTH_USER_NAME);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void 참여하는_사람의_이름이_5글자를_넘는_경우(){
-		final String name = "";
+		final String name = new StringBuilder().append(MAX_LENGTH_USER_NAME).append(MIN_LENGTH_USER_NAME).toString();
 		new User(name);
 	}
 	
