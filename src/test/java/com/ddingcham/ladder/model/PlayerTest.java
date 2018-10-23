98fca2be3dfbe679cf1a7b32a31a8ae692adf79a
ddingcham <qo0op308@gmail.com>
2018-10-23 13:49:10 +0900

    사람 이름은 쉼표를 기준으로 구분한다 한 명이라 쉼표가 없는 경우

--- src/test/java/com/ddingcham/ladder/model/PlayerTest.java
+++ src/test/java/com/ddingcham/ladder/model/PlayerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class PlayerTest {
 
 	@Test
-	public void 사람_이름은_쉼표를_기준으로_구분한다(){
+	public void 사람_이름은_쉼표를_기준으로_구분한다_다섯명(){
 		final Player[] expectedPlayers = new Player[]{
 				new Player("이"),
 				new Player("름"),
@@ -14,6 +14,16 @@ public class PlayerTest {
 				new Player("섯"),
 				new Player("명")};
 		final String testTarget = new String("이,름,다,섯,명");
+		
+		Assert.assertArrayEquals(expectedPlayers, Player.generatePlayer(testTarget));
+	}
+	
+	@Test
+	public void 사람_이름은_쉼표를_기준으로_구분한다_한_명_이라_쉼표가_없는_경우(){
+		final Player[] expectedPlayers = new Player[]{
+				new Player("이름")
+		};
+		final String testTarget = new String("이름");
 		
 		Assert.assertArrayEquals(expectedPlayers, Player.generatePlayer(testTarget));
 	}
